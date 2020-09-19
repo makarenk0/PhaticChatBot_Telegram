7e9d97c3298a7e5d4e13158e1c72bc34e3cf6652
makarenk0 <45842815+makarenk0@users.noreply.github.com>
2020-09-19 16:00:49 +0300

    Explanation

--- PhaticChatBot_Telegram/Models/AppSettings.cs
+++ PhaticChatBot_Telegram/Models/AppSettings.cs
@@ -7,10 +7,10 @@ namespace PhaticChatBot_Telegram.Models
 {
     static public class AppSettings
     {
-        public static string Url { get; set; } = "https://phaticchatbottelegram.azurewebsites.net:443/{0}";
+        public static string Url { get; set; } = ""; //url to server
 
-        public static string Name { get; set; } = "PhaticChatBot";
+        public static string Name { get; set; } = ""; //bot name
 
-        public static string Key { get; set; } = "1200498122:AAEYN_smwdCZC8SP8DIofYeV3Bc1BXVHNSg";
+        public static string Key { get; set; } = ""; //telegram api key
     }
 }
